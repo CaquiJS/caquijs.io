d9dbb225f5a62dc5da877ae4855105fb400f92c9
Renan Vaz <renan.c.vaz@gmail.com>
2014-01-27 04:46:27 -0200

    Working...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+$(function(){
+    var scrollLastPos = $(window).scrollTop();
+    $(window).on('scroll', function(){
+        var scrollCurrentPos = $(window).scrollTop();
+
+        // if scrolling to down
+        if (scrollCurrentPos > scrollLastPos) {
+            $('header .secondary-content').addClass('offscreen');
+        } else {
+            $('header .secondary-content').removeClass('offscreen');
+        }
+
+        scrollLastPos = scrollCurrentPos;
+    });
+});
