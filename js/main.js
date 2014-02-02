0c438628b754805bd1be2544d2528cf2700a7dec
Renan Vaz <renan.c.vaz@gmail.com>
2014-02-01 22:27:38 -0200

    CSS rows

--- js/main.js
+++ js/main.js
@@ -4,13 +4,34 @@ $(function(){
     $(window).on('scroll', function(){
         var scrollCurrentPos = $(window).scrollTop();
 
+        // Esta variável só deve ser calculada se houver inserção de novos itens, isto aqui é um teste.
+        var lastNewItemPos = $('main .alert-warning').last().offset().top - $('header').height();
+
         // if scrolling to down
         if (scrollCurrentPos > scrollLastPos) {
-            $('header .secondary-content').addClass('offscreen');
+            $('[data-offscreen]').addClass('offscreen');
         } else {
-            $('header .secondary-content').removeClass('offscreen');
+            $('[data-offscreen]').removeClass('offscreen');
+        }
+
+        if (scrollCurrentPos < lastNewItemPos) {
+            lastNewItemPos = 0;
+            $('.alert-news').removeClass('has-news');
+        }
+
+        if (scrollCurrentPos >= ($(document).height() - $(window).height()) - 200) {
+            $('.loading-more').css({opacity: 1});
         }
 
         scrollLastPos = scrollCurrentPos;
     });
+
+    $('.alert-news').on('click', function(){
+        $(this).removeClass('has-news');
+        $('html, body').animate({scrollTop: $('main section').offset().top - $('header').height() - 15}, 600);
+    });
+
+    setTimeout(function(){
+        $('.alert-news').addClass('has-news');
+    }, 5000);
 });
